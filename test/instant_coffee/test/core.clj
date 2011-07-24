3d2dbff47e035b8698a2aba0f42907b896bd2588
Gary Fredericks <fredericksgary@gmail.com>
2011-07-24 17:52:52 -0400

    Maybe we can keep those numbers low

--- test/instant_coffee/test/core.clj
+++ test/instant_coffee/test/core.clj
@@ -1,8 +1,12 @@
 (ns instant-coffee.test.core
+  (:require [instant-coffee.jcoffeescript :as jc])
   (:use clojure.test
         instant-coffee.test.helpers
         instant-coffee.config
         instant-coffee.core))
+
+; warm up the compiler
+(jc/compile-coffee "x=5")
 
 (def-fs-test basic-coffee-compile-test
   (spit (file "coffeescripts/fumble.coffee") "x = 'Wallaby'")
@@ -11,29 +15,30 @@
     (is (.exists (file "public/javascripts" (str filename ".js"))))))
 
 (def-watcher-test basic-coffee-watch-test
+  (Thread/sleep 700)
   (let [tgt (file "public/javascripts/fumble.js")]
     (is (not (.exists tgt)))
     (spit (file "coffeescripts/fumble.coffee") "x = 'Wallaby'")
-    (Thread/sleep 3000)
+    (Thread/sleep 700)
     (is (.exists tgt))
     (is (re-find #"Wallaby" (slurp tgt)))
     (spit (file "coffeescripts/fumble.coffee") "x = 'Horton'")
-    (Thread/sleep 1500)
+    (Thread/sleep 800)
     (is (.exists tgt))
     (is (not (re-find #"Wallaby" (slurp tgt))))
     (is (re-find #"Horton" (slurp tgt)))))
 
 (def-watcher-test deleting-test
-  (Thread/sleep 2000)
+  (Thread/sleep 700)
   (let [tgt (file "public/javascripts/bar.js")]
     (is (.exists tgt))
     (is (.exists (file "coffeescripts/bar.coffee")))
     (.delete (file "coffeescripts/bar.coffee"))
-    (Thread/sleep 1000)
+    (Thread/sleep 800)
     (is (not (.exists tgt)))))
 
 (def-watcher-test compile-error-test
-  (Thread/sleep 2000)
+  (Thread/sleep 700)
   (let [src (file "coffeescripts/bar.coffee"),
         tgt (file "public/javascripts/bar.js")]
     (is (.exists tgt))
