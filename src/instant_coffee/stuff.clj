2fbcf2475319e80a638fe05780e51a5f52b2ac2d
Gary Fredericks <fredericksgary@gmail.com>
2011-07-24 22:27:12 -0400

    Extract the file-watching algorithm. Much cleaner now.

--- src/instant_coffee/stuff.clj
+++ src/instant_coffee/stuff.clj
@@ -62,48 +62,66 @@
                 (cache-set-error cache src-hash (.getMessage e))
                 (throw+ {:compile (.getMessage e)}))))))))
 
+(defn- file-watcher
+  "Takes three functions -- a file finder (a nullary function that returns a
+  list of filenames) and two file-handlers. The first handler is a two-arg
+  function that takes a filename and the file's contents and does something with
+  it. The second takes a filename, and is called when the file has been deleted.
+  file-watcher returns a function that, when invoked, will fetch the files and
+  call file-handler for any files which have changed since the last time
+  file-handler was called."
+  [src-dir file-finder change-handler delete-handler]
+  (let [last-compiled* (atom {}),
+        last-compiled (fn [filename] (-> last-compiled* deref (get filename) first)),
+        last-compiled-value (fn [filename] (-> last-compiled* deref (get filename) second))]
+    (fn []
+      (let [srcs (set (file-finder))
+            deleted-srcs (sets/difference (-> last-compiled* deref keys set) srcs)]
+        (doseq [src-filename srcs]
+          (let [src-file (file src-dir src-filename)]
+            (when (or (nil? (last-compiled src-filename))
+                    (FileUtils/isFileNewer
+                      src-file
+                      (round-down-milliseconds (last-compiled src-filename))))
+              (let [slurped-at (now),
+                    src (slurp (file src-dir src-filename)),
+                    hashed-src (sha1 src)]
+                (when-not (= hashed-src (last-compiled-value src-filename))
+                  (swap! last-compiled* assoc src-filename [slurped-at hashed-src])
+                  (change-handler src-filename src))))))
+        (doseq [src-filename deleted-srcs]
+          (swap! last-compiled* dissoc src-filename)
+          (delete-handler src-filename))))))
+
 (defmethod subcompiler :coffeescript
   [_ coffee-config]
   (let [{src-dir :src, target-dir :target} coffee-config,
-        last-compiled* (atom {}),
-        last-compiled (fn [filename] (-> last-compiled* deref (get filename) first)),
-        last-compiled-value (fn [filename] (-> last-compiled* deref (get filename) second)),
         maybe-compile (create-cached-coffeescript-compiler)]
-    (fn []
-      (let [srcs (set (source-files "coffee" src-dir))
-            deleted-srcs (sets/difference (-> last-compiled* deref keys set) srcs)]
-        (doseq [coffee srcs]
-          (let [src-file (file src-dir coffee)]
-            (when (or (nil? (last-compiled coffee))
-                    (FileUtils/isFileNewer
-                      src-file
-                      (round-down-milliseconds (last-compiled coffee))))
-              (let [target-filename (string/replace coffee #"\.coffee$" ".js"),
-                    target-file (file target-dir target-filename),
-                    slurped-at (now)]
-                (try+
-                  (let [target-dir (.getParentFile target-file),
-                        src (slurp (file src-dir coffee)),
-                        hashed-src (sha1 src)]
-                    (when-not (= hashed-src (last-compiled-value coffee))
-                      (when-not (.exists target-dir)
-                        (.mkdirs target-dir))
-                      (swap! last-compiled* assoc coffee [slurped-at hashed-src])
-                      (print (format "Compiling %s..." coffee))
-                      (.flush *out*)
-                      (spit target-file (maybe-compile src hashed-src))
-                      (print "\n")
-                      (.flush *out*)))
-                  (catch #(and (map? %) (contains? % :compile)) {msg :compile}
-                    (println "Error! " msg)
-                    (if (.exists target-file)
-                      (.delete target-file))))))))
-        (doseq [coffee deleted-srcs]
-          (let [target-file (file target-dir (string/replace coffee #"\.coffee$" ".js"))]
-            (when (.exists target-file)
-              (println (format "Deleting %s..." coffee))
-              (.delete target-file))
-            (swap! last-compiled* dissoc coffee)))))))
+    (file-watcher
+      src-dir
+      (partial source-files "coffee" src-dir)
+      (fn [coffee src]
+        (let [target-filename (string/replace coffee #"\.coffee$" ".js"),
+              target-file (file target-dir target-filename)]
+          (try+
+            (let [target-dir (.getParentFile target-file),
+                  hashed-src (sha1 src)]
+              (when-not (.exists target-dir)
+                (.mkdirs target-dir))
+              (print (format "Compiling %s..." coffee))
+              (.flush *out*)
+              (spit target-file (maybe-compile src hashed-src))
+              (print "\n")
+              (.flush *out*))
+            (catch #(and (map? %) (contains? % :compile)) {msg :compile}
+              (println "Error! " msg)
+              (if (.exists target-file)
+                (.delete target-file))))))
+      (fn [coffee]
+        (let [target-file (file target-dir (string/replace coffee #"\.coffee$" ".js"))]
+          (when (.exists target-file)
+            (println (format "Deleting %s..." coffee))
+            (.delete target-file)))))))
 
 (defmethod subcompiler :haml
   [_ haml-config]
