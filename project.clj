39e6d29216e78fc7965bf64b487318c1b6d9f7ba
Gary Fredericks <fredericksgary@gmail.com>
2011-10-20 11:10:05 -0400

    clj-yaml got a proper release in clojars

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [com.gfredericks/jcoffeescript "1.1.0"]
-                 [gfrlog/clj-yaml "0.3.0"]
+                 [clj-yaml "0.3.1"]
                  [commons-io/commons-io "2.0.1"]
                  [org.clojure/data.json "0.1.1"]
                  [slingshot "0.2.0"]]
